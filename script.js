4c2435aa258de7c3c2625f8981b5847269389b80
Steve Fallet <steve.fallet@divtec.ch>
2024-09-17 13:54:39 +0200

    mise à jour stat, supression complète de Vue

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Importation de la Composition API de Vue.js
-const { createApp } = Vue;
+/**
+ * Intro à vue.js
+ * @author Steve Fallet
+ * @date 2024-09-17
+ */
 
-createApp({
-  // La fonction setup est utilisée pour définir l'état réactif et les fonctions
-  setup() {
-    // Message d'accueil affiché au chargement de l'application
-    const message = "Bienvenue dans PokeCount!";
-
-    // Retourne les variables et fonctions pour qu'elles soient accessibles dans le template HTML
-    return {
-      message, // Le message d'accueil
-    };
-  }
-}).mount("#poke-app"); // Monte l'application Vue.js sur l'élément HTML avec l'ID 'poke-app'
+console.log('Intro à vue.js');
